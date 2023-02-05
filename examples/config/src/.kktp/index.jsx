eece4344d1bf3502a8a2cf723e374a6756e65a07
SunLxy <1011771396@qq.com>
2023-02-05 21:08:39 +0800

    fix:忽略文件夹

--- examples/config/src/.kktp/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactClient from 'react-dom/client';
-import './index.css';
-import Route from './routes';
-
-ReactClient.createRoot(document.getElementById('root')).render(<Route />);
