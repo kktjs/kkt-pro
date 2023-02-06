b76ec671e0c2b10d1c961e1d59a9012434f72362
SunLxy <1011771396@qq.com>
2023-02-06 16:08:31 +0800

    fix:引入配置取值问题

--- core/src/utils/getLoadConfig.ts
+++ core/src/utils/getLoadConfig.ts
@@ -4,5 +4,5 @@ import defaultConfig from './config';
 export const getLoadConfig = async () => {
   load.use(defaultConfig);
   const config = await load('.kktp');
-  return config.value;
+  return config.raw;
 };
