1b4b3b9d44c9f8483e6aa07ff9d44e4991ff3262
SunLxy <1011771396@qq.com>
2023-02-12 11:51:56 +0800

    fix:修复配置文件不存在报错

--- core/src/utils/getLoadConfig.ts
+++ core/src/utils/getLoadConfig.ts
@@ -2,6 +2,6 @@ import load from 'carefree-proload';
 import defaultConfig from './config';
 export const getLoadConfig = async () => {
   load.use(defaultConfig);
-  const config = await load('.kktp');
-  return config.raw;
+  const config = await load('.kktp', { mustExist: false });
+  return (config || {}).raw || {};
 };
