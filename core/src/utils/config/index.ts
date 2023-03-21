6942d7961a4c49583f94640fd28bfb859507533d
姚宾 <yaob421123@163.com>
2023-03-21 15:07:32 +0800

    released v1.0.6-bate.1

--- core/src/utils/config/index.ts
+++ core/src/utils/config/index.ts
@@ -8,24 +8,30 @@ const defaultConfig = [
     extensions: ['js', 'jsx', 'ts', 'tsx', 'cts', 'mts'],
     async register(fileName: string) {
       // 为了解决 js 文件 使用 export default 导出 (配置中使用 import 引入包有点问题)
+      const { register } = require('@swc-node/register/register');
       if (/\.(js|jsx|ts|tsx?)$/.test(fileName)) {
-        const { register } = require('@swc-node/register/register');
         register({
           esModuleInterop: true,
           module: 'esnext',
           format: 'esm',
           extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          ignore: [/\/(node_modules)\//],
         });
       } else if (/\.([cm]ts|tsx?)$/.test(fileName)) {
         const { register } = require('@swc-node/register/register');
         if (fileName.endsWith('.cts')) {
-          register({ format: 'cjs', extensions: ['.cts'] });
+          register({
+            format: 'cjs',
+            extensions: ['.cts'],
+            ignore: [/\/(node_modules)\//],
+          });
         } else {
           register({
             esModuleInterop: true,
             module: 'esnext',
             format: 'esm',
             extensions: ['.ts', '.tsx', '.mts'],
+            ignore: [/\/(node_modules)\//],
           });
         }
       }
