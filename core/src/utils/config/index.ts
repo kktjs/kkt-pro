78f5b939452180ac58d060dd5270f9bbda9004fd
姚宾 <yaob421123@163.com>
2023-03-22 13:05:41 +0800

    fix: swc replace to babel

--- core/src/utils/config/index.ts
+++ core/src/utils/config/index.ts
@@ -8,30 +8,26 @@ const defaultConfig = [
     extensions: ['js', 'jsx', 'ts', 'tsx', 'cts', 'mts'],
     async register(fileName: string) {
       // 为了解决 js 文件 使用 export default 导出 (配置中使用 import 引入包有点问题)
-      const { register } = require('@swc-node/register/register');
+      const register = require('@babel/register');
       if (/\.(js|jsx|ts|tsx?)$/.test(fileName)) {
         register({
-          esModuleInterop: true,
-          module: 'esnext',
-          format: 'esm',
           extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          presets: ['env'],
           ignore: [/\/(node_modules)\//],
         });
       } else if (/\.([cm]ts|tsx?)$/.test(fileName)) {
-        const { register } = require('@swc-node/register/register');
         if (fileName.endsWith('.cts')) {
           register({
             format: 'cjs',
             extensions: ['.cts'],
+            presets: ['env'],
             ignore: [/\/(node_modules)\//],
           });
         } else {
           register({
-            esModuleInterop: true,
-            module: 'esnext',
-            format: 'esm',
             extensions: ['.ts', '.tsx', '.mts'],
             ignore: [/\/(node_modules)\//],
+            presets: ['env'],
           });
         }
       }
